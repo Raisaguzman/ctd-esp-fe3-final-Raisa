3d2d3e585d029a3ac67906242ac3bc0ba8f244a0
Raisaguzman <raisa1005@gmail.com>
2024-12-06 14:01:44 -0500

    detalles finales

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,20 @@
+import "../index.css";
 
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
+      <p>Powered by</p>
+      <div>
+        <img src="./images/DH.png" alt="DH-logo" className="logo-DH"/>
+        <div>
+          <img src="./images/ico-facebook.png" alt="facebook" className="redes-sociales"/>
+          <img src="./images/ico-instagram.png" alt="instagram"  className="redes-sociales"/>
+          <img src="./images/ico-tiktok.png" alt="tiktok" className="redes-sociales" />
+          <img src="./images/ico-whatsapp.png" alt="whatsapp"  className="redes-sociales"/>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,12 +11,14 @@ const Navbar = () => {
     <nav className={`navbar ${state.theme}`}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <h2>DH Odonto</h2>
       <div>
         <Link to="/">Home</Link>
         <Link to="/favs">Favs</Link>
         <Link to="/contact">Contact</Link>
+        <ThemeToggleButton />
       </div>
-      <ThemeToggleButton />
+      
     </nav>
   );
 };
